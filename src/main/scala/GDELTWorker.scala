130f0d1c9bdf6cfb2ba4a50a7171414ed41c9256
Adel <mradel700@gmail.com>
2017-10-03 13:03:41 +0300

    Practice 2 completed.

--- src/main/scala/GDELTWorker.scala
+++ src/main/scala/GDELTWorker.scala
@@ -2,19 +2,27 @@ import org.apache.spark.sql.SparkSession
 
 object GDELTWorker {
   def main(args: Array[String]): Unit = {
-
-    var gdeltFile = "C:/dataForScalaProjects/gdelt.csv";
-
+    val gdeltFile = "C:/dataForScalaProjects/gdelt.csv"
+    val cameoFile = "C:/dataForScalaProjects/CAMEO_event_codes.csv"
     //Initialize SparkSession
     val sparkSession = SparkSession
       .builder()
       .appName("spark-sql-basic")
       .master("local[*]")
       .getOrCreate()
-
     //Initialize GDELT DataFrame
     val gdelt = new GDELT(sparkSession, gdeltFile)
+    //gdelt.mostMentionedActors().show(50)
 
-    gdelt.mostMentionedActors().show(50)
+    val mostMentionedEvents = gdelt.mostMentionedEvents()
+    val cameoCodes = sparkSession.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .option("delimiter", "\t")
+      .csv(cameoFile)
+    mostMentionedEvents.join(cameoCodes,
+      mostMentionedEvents.col("EventCode") === cameoCodes.col("CAMEOcode"), "inner")
+      .select("EventCode", "EventDescription", "count")
+      .show(25)
   }
 }
